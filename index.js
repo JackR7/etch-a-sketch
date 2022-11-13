a261055f179f15bf241fc7205c862278e6a039e1
JackR7 <apaux7@gmail.com>
2022-11-13 17:20:47 +0100

    adding grid event listenere

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 let container = document.getElementsByClassName('grid-container')[0];
+let gridItemDivArray = [];
+//const changeColor = e => {
+  //  e.target.style.backgroundColor = 'red';
+//}
 
 for(let i = 1; i <= 256; i++){
-    let gridItem = document.createElement('div');
-    gridItem.className = 'grid-item';
-    //gridItem.textContent = `${i}`;
-    container.appendChild(gridItem);
+    let gridItemDiv = document.createElement('div');
+    gridItemDiv.id = 'grid-itemid';
+    gridItemDiv.textContent = `${i}`;
+    container.appendChild(gridItemDiv);
+    
 }
 
+let gridItemId = document.getElementById('grid-itemid');
+let gridItems = document.querySelectorAll('#grid-itemid');
+
+gridItems.forEach(gridItem => {
+    gridItem.addEventListener('click', () => changeColorBackground(gridItem));
+});
+
+function changeColorBackground(gridItem){
+    gridItem.style.backgroundColor = 'red'
+}
