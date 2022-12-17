475fc4bbbdb5b85db793f785ea8599c90b61e2a0
JackR7 <apaux7@gmail.com>
2022-12-17 14:08:03 +0100

    adding something

--- index.js
+++ index.js
@@ -4,25 +4,38 @@ let gridItemDivArray = [];
   //  e.target.style.backgroundColor = 'red';
 //}
 
+
 for(let i = 1; i <= 256; i++){
     let gridItemDiv = document.createElement('div');
     gridItemDiv.id = 'grid-itemid';
-    gridItemDiv.textContent = `${i}`;
     container.appendChild(gridItemDiv);
     
 }
 
 let gridItemId = document.getElementById('grid-itemid');
 let gridItems = document.querySelectorAll('#grid-itemid');
-
+let btn = document.querySelector('#btn');
 
 gridItems.forEach(gridItem => {
     gridItem.addEventListener('click', () => changeColorBackground(gridItem));
-    gridItem.addEventListener('mouseover', () => console.log("mouseover"));
+    gridItem.addEventListener('click', function (e) {
+        console.log(e);
+      });
+
+      let mouseType;
+      gridItem.addEventListener('click', function(e){mouseType = e.type});
+      if(mouseType === 'mousedown'){
+        gridItem.addEventListener('mouseover', () => changeColorBackground(gridItem));
+      }
+      
 });
 
+btn.addEventListener('click', function (e) {
+    console.log(e);
+  });
 
 function changeColorBackground(gridItem){
-    gridItem.style.backgroundColor = 'red'
+    gridItem.style.backgroundColor = 'red';
 }
+
 
